9e508e74c1399407ad9b947fcbf47d400d4da3dc
Himanshu bisht <himbisht845@gmail.com>
2021-01-12 14:22:25 +0530

    broken  ou homepage into smaller components,finished styling our menu-item componentr

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 // import logo from './logo.svg';
 import './App.css';
-import Homepage from './Homepage.container'
+import Homepage from './pages/homepage/homepage.container.jsx';
+
 
 function App() {
   return (
